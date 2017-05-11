185c0f88f86899eb8d4cc20b5958b232824ad85d
Joseph Ball <Joseph Ball>
2017-05-10 20:23:22 -0400

    Added on click function for submit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,45 @@
+$(document).ready(function() {
+	$('select').material_select();
+});
+
+var userName = "";
+var twitterHandle = "";
+var dogName = "";
+var dogBreed = "";
+var dogTemp = "";
+var dogAge = "";
+
+
+$("#submit-info").on("click", function() {
+	event.preventDefault();
+
+	var dogTempDrop = $("#dog_temp");
+	var dogAgeDrop = $("#dog_age");
+
+	userName = $("#full_name").val().trim();
+	twitterHandle = $("#twitter_handle").val().trim();
+	dogName = $("#dog_name").val().trim();
+	dogBreed = $("#dog_breed").val().trim();
+	dogTemp = $("#dog_temp").val();
+	dogAge = $("#dog_age").val();
+
+	console.log("userName: "+userName);
+	console.log("twitterHandle: "+twitterHandle);
+	console.log("dogName: "+dogName);
+	console.log("dogBreed: "+dogBreed);
+	console.log("dogTemp: "+dogTemp);
+	console.log("dogAge: "+dogAge);
+
+	$("#mainContent").empty();
+	$("#mainContent").addClass("map");
+
+	initMap();
+});
+
+
+
+
+
 // Note: This example requires that you consent to location sharing when
 // prompted by your browser. If you see the error "The Geolocation service
 // failed.", it means you probably did not give permission for the browser to
@@ -5,7 +47,7 @@
 var map, infoWindow;
 
 function initMap() {
-	map = new google.maps.Map(document.getElementById('googleMap'), {
+	map = new google.maps.Map(document.getElementById('mainContent'), {
 		center: {lat: 40.712, lng: -74.0059},
 		zoom: 8
 	});
