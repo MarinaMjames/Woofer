fb74dad0d623ef6c1c7267ebca5d841e4ff2c937
Andrew Sadowski <Andrew Sadowski>
2017-05-15 15:33:31 -0400

    created for loop and updated variables from the petfinder api to work within the for loop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 	var yourKey = "key=07e7adfc27bd9872413e0961018c8013&";
 	var baseURL = "https://api.petfinder.com/";
@@ -11,72 +11,79 @@
 	var searchAge = "age=Young&"
 	var format = "format=json";
 
-
+	var dogsReturned = []
 	
 	// https://api.petfinder.com/pet.find?animal-dog&count=15&key=07e7adfc27bd9872413e0961018c8013&format=json
 	var fullURL = baseURL+reqType+dogSearch+searchCount+searchLocation
 	+searchSex+searchSize+searchAge+yourKey+format;
+	
+
+
+function renderDogs() {	
 	$.ajax({ 
 	  method: 'GET', 
 	  url: fullURL + '&callback=?', 
 	  dataType: 'json', 
 	  success: function(data) { 
 	    // console.log(data);
-	    var x = JSON.stringify(data) 
-	    $(".output").html(x); 
+	    // var x = JSON.stringify(data) 
+	    // $("#listOfDogs").html(x); 
 	    
+	    $("#listOfDogs").empty()
 	    // object for the first returned pet stored
 	    // in a variable
-		var foundPet = data.petfinder.pets.pet[0]
-			// console.log("PET: " + foundPet)
-		// stores contact object in a varible for easier
-		// access
-		var petContact = foundPet.contact
-		// stores phone number to contact 
-		// shelter for pet in variable
-			console.log("------------------")
-			console.log("--Location Info---")
-		var petPhone = petContact.phone.$t
-			console.log("PET PHONE: " + petPhone) 
-		// stores email to contact
-		// shelter for pet in variable
-		var petEmail = petContact.email.$t
-			console.log("PET EMAIL: " + petEmail) 
-		// stores city pet is located in a variable
-		var petCity = petContact.city.$t
-			console.log("PET CITY: " + petCity)
-		// stores zip code of pet's city 
-		// in a varible
-		var petZip = petContact.zip.$t
-			console.log("PET ZIP: " + petZip)
-			
-		// stores age of pet in a variable
-			console.log("-----Pet Info-----")
-		var petAge = foundPet.age.$t
-			console.log("PET AGE: " + petAge)
-		// stores size of pet in a variable
-		var petSize = foundPet.size.$t
-			console.log("PET SIZE: " + petSize)
-		// stores name of pet in a variable
-		var petName = foundPet.name.$t
-			console.log("PET NAME: " + petName)
-		// stores gender of pet in a variable
-		var petSex = foundPet.sex.$t
-			console.log("PET SEX: " + petSex)
-			
-		// stores image of pet in a variable
-		var petImage = foundPet.media.photos.photo[7].$t
-			console.log("PET IMAGE: " + petImage)
-		// stores Shelter ID for pet in a variable 
-		var petShelterID = foundPet.shelterId.$t
-			console.log("SHELTER ID: " + petShelterID)
-			console.log("------------------")
+		var foundPet = data.petfinder.pets.pet
+			// data.petfinder.pets.pet[0]
+			for (var i = 0; i < foundPet.length; i++){
+				var petContact = foundPet[i].contact
+			// stores phone number to contact 
+			// shelter for pet in variable
+				
+			var petPhone = petContact.phone.$t
+				
+			// stores email to contact
+			// shelter for pet in variable
+			var petEmail = petContact.email.$t
+				
+			// stores city pet is located in a variable
+			var petCity = petContact.city.$t
+				
+			// stores zip code of pet's city 
+			// in a varible
+			var petZip = petContact.zip.$t
+				
+				
+			// stores age of pet in a variable
+				
+			var petAge = foundPet[i].age.$t
+				
+			// stores size of pet in a variable
+			var petSize = foundPet[i].size.$t
+				
+			// stores name of pet in a variable
+			var petName = foundPet[i].name.$t
+				
+			// stores gender of pet in a variable
+			var petSex = foundPet[i].sex.$t
+				
+			var petDiv = $("<div class='petDiv'>");	
+			// stores image of pet in a variable
+			var petImage = $("<img>")
+			petImage.attr("src", foundPet[i].media.photos.photo[7].$t)
+			petDiv.append(petImage);
+				 console.log(petImage)
+			// stores Shelter ID for pet in a variable 
+			var petShelterID = foundPet[i].shelterId.$t
+				
+				}	
+			// stores contact object in a varible for easier
+			// access
+		
 		
 		}
 
 	  });
-
-=======
+}
 // Initialize Firebase
 var config = {
 	apiKey: "AIzaSyCZ5kHNolNVZx831g9c-2ivxTlCYoknJ0s",
@@ -100,10 +107,10 @@ $(document).ready(function() {
 // variables to store user info from form
 
 var zip = "";
-var dogBreed = "";
-var dogGender = "";
-var dogAge = "";
-var dogSize = "";
+// var dogBreed = "";
+// var dogGender = "";
+// var dogAge = "";
+// var dogSize = "";
 
 
 // on click function for form submit button
@@ -112,18 +119,18 @@ $("#submit-info").on("click", function() {
 
 	// set user input to appropriate variables
 
-	zip = $("#zip_code").val().trim();
-	dogBreed = $("#dog_breed").val().trim();
-	dogGender = $("#dog_gender").val();
-	dogAge = $("#dog_age").val();
-  dogSize = $("#dog_size").val();
+	// zip = $("#zip_code").val().trim();
+	// dogBreed = $("#dog_breed").val().trim();
+	// dogGender = $("#dog_gender").val();
+	// dogAge = $("#dog_age").val();
+ //  dogSize = $("#dog_size").val();
 
 
-	console.log("dogBreed: "+dogBreed);
-	console.log("dogGender: "+dogGender);
-	console.log("dogAge: "+dogAge);
-  console.log("dogSize: "+dogSize);
-  console.log("zipCode: " +zip);
+	// console.log("dogBreed: "+dogBreed);
+	// console.log("dogGender: "+dogGender);
+	// console.log("dogAge: "+dogAge);
+ //  console.log("dogSize: "+dogSize);
+ //  console.log("zipCode: " +zip);
 
 
 	// empty mainContent div and append a div for the map to it
@@ -133,6 +140,7 @@ $("#submit-info").on("click", function() {
 
 	// run function to display map
 	googleMap.initMap();
+	renderDogs();
 });
 
 // object to handle Google Maps API
@@ -248,4 +256,3 @@ var googleMap = {
 		});
 	},
 }
->>>>>>> 92c92d707378e7227edbb4be66cebd53b4d4aedb
