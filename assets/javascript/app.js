137b4994033dfc4f0670cbe490012a080fdb5603
Marina James <MarinaJames@Marinas-MacBook-Air.local>
2017-05-19 23:28:18 -0400

    adjusted scrolling time, made text scroll using GSAP

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -752,26 +752,27 @@ var favorites = {
 favorites.getKey();
 
 
-var $holder = $(".holder");
-var $list = $holder.find("ul.list");
-var $clonedList = $list.clone();
+var $scrolledText = $(".scrollingText");
+var $word = $scrolledText.find("ul.words");
+var $clonedList = $word.clone();
 
-var listWidth = $list.find("li").length * 200;
-var endPos = $holder.width() - listWidth;
+var listWidth = $word.find("li").length * 400;
+var endPos = $scrolledText.width() - listWidth;
 
-$list.add($clonedList).css({
+$word.add($clonedList).css({
 	"width" : listWidth + "px"
 });
 
-$clonedList.addClass("cloned").appendTo($holder);
+$clonedList.addClass("cloned").appendTo($scrolledText);
 
 //TimelineMax
 var infinite = new TimelineMax({repeat: -1, paused: false});
-var time = 10;
+var time = 15;
 
-infinite.fromTo($list, time, {left:0}, {left: -listWidth, ease: Linear.easeNone}, 0);
+infinite.fromTo($word, time, {left:0}, {left: -listWidth, ease: Linear.easeNone}, 0);
 infinite.fromTo($clonedList, time, {left:listWidth}, {left:0, ease: Linear.easeNone}, 0);
-infinite.set($list, {left: listWidth});
+infinite.set($word, {left: listWidth});
 infinite.to($clonedList, time, {left: -listWidth, ease: Linear.easeNone}, time);
-infinite.to($list, time, {left: 0, ease: Linear.easeNone}, time);
+infinite.to($word, time, {left: 0, ease: Linear.easeNone}, time);
 
+ 
